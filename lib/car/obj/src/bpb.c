07c98defe9375534a6f6100cccd5eb6fb9fab96f
chris <chlds@yahoo.com>
2025-05-12 12:43:55 +0900

    Fix

---
+++ lib/car/obj/src/bpb.c
@@ -0,0 +1,15 @@
+/* Synopsis
+
+Bits per byte
+*/
+
+
+# define CAR_H
+# include "./incl/config.h"
+
+signed(__cdecl bpb(void)) {
+auto signed char r;
+AND(r,0x00);
+NOT(r);
+return(bpb_r(&r));
+}
--- lib/car/obj/src/incl/config.h
+++ lib/car/obj/src/incl/config.h
@@ -1,13 +1,21 @@
 // # define __cdecl
-# define COMPILING_ON_WSL_H // e.g., for WSL running on x86
-
 # define global
 # define external extern
+
+# define AL_H
+# define COMPILING_ON_WSL_H // e.g., for WSL running on x86
 
 # ifdef COMPILING_ON_WSL_H
 # ifndef READY_FOR_COMPILING_ON_WSL_H
 # define READY_FOR_COMPILING_ON_WSL_H
 # define __cdecl __attribute__((ms_abi))
+# endif
+# endif
+
+# ifdef AL_H
+# ifndef READY_FOR_AL_H
+# define READY_FOR_AL_H
+# include "./al.h"
 # endif
 # endif
 
