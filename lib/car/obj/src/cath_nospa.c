a5738ab2d881c7bfba76c512d5d3fa03e9a141e5
chris <chlds@yahoo.com>
2025-05-28 17:24:05 +0900

    Add

---
+++ lib/car/obj/src/cath_nospa.c
@@ -0,0 +1,12 @@
+# define CAR_H
+# include "./incl/config.h"
+
+signed char *(__cdecl cath_nospa(signed char(*argp))) {
+auto signed char *b;
+auto signed r;
+b = cat_nospa(argp);
+r = cue_spa(b);
+if(none(r)) return(b);
+bury(r+(b));
+return(cathy_xe(b));
+}
