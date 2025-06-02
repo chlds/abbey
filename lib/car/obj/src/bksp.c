7167bf71d2f04260b6024951b1b36d592e666338
chris <chlds@yahoo.com>
2025-06-02 21:54:10 +0900

    Remove

--- lib/car/obj/src/bksp.c
+++
@@ -1,6 +0,0 @@
-# define CAR_H
-# include "./incl/config.h"
-
-signed char *(__cdecl bksp(signed char(*argp))) {
-return(rm_by_lt(argp));
-}
--- lib/car/obj/src/bksp_xe.c
+++
@@ -1,6 +0,0 @@
-# define CAR_H
-# include "./incl/config.h"
-
-signed char *(__cdecl bksp_xe(signed char(*argp))) {
-return(rm_by_lt_xe(argp));
-}
