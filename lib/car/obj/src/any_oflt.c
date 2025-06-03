c5a3e98959f05dbbaa26e44113cbe722e0d79899
chris <chlds@yahoo.com>
2025-06-03 14:10:10 +0900

    Add

---
+++ lib/car/obj/src/any_oflt.c
@@ -0,0 +1,6 @@
+# define CAR_H
+# include "./incl/config.h"
+
+signed(__cdecl any_oflt(signed char(*argp))) {
+return(any_ofnomb(argp));
+}
---
+++ lib/car/obj/src/any_ofnolt.c
@@ -0,0 +1,6 @@
+# define CAR_H
+# include "./incl/config.h"
+
+signed(__cdecl any_ofnolt(signed char(*argp))) {
+return(any_ofmb(argp));
+}
