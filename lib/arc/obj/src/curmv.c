55e2bb7c1b0432539cddf6d1c65a5f76dc02502c
chris <chlds@yahoo.com>
2025-06-15 22:50:55 +0900

    Fix

--- lib/arc/obj/src/curmv.c
+++ lib/arc/obj/src/curmv.c
@@ -1,3 +1,9 @@
+/* Synopsis
+
+CUP: Cursor Position
+*/
+
+
 # define ARC_H
 # define CAR_H
 # define UNISTD_H
@@ -8,8 +14,10 @@ auto signed char *b;
 auto signed r;
 auto signed char a[] = (";");
 auto signed char h[] = ("H");
+if(!(0x00<(args))) args = (0x01);
+if(!(0x00<(argp))) argp = (0x01);
 r = (STDOUT_FILENO);
-b = catna(a,catha(ARC_H_ESCAPE,catha(ARC_H_CONTROL_SEQUENCE_INTRODUCER,cv_dec(args))));
+b = catna(a,catha(ARC_H_CONTROL_SEQUENCE_INTRODUCER,cv_dec(args)));
 b = catna(h,cathe(b,cv_dec(argp)));
 r = wrs(r,b,ct(b));
 rlse(b);
--- lib/arc/obj/src/incl/arc_d.h
+++ lib/arc/obj/src/incl/arc_d.h
@@ -1,2 +1,2 @@
-static signed char ARC_H_CONTROL_SEQUENCE_INTRODUCER[] = ("[");
+static signed char ARC_H_CONTROL_SEQUENCE_INTRODUCER[] = ("\033[");
 static signed char ARC_H_ESCAPE[] = ("\033");
