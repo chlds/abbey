b89aa633e8e4ddc1071da87923db538ad4c1c61d
chris <chlds@yahoo.com>
2025-06-18 12:56:50 +0900

    Fix

--- lib/arc/obj/src/el_entire.c
+++ lib/arc/obj/src/el_entire.c
@@ -13,8 +13,9 @@ signed(__cdecl el_entire(void)) {
 auto signed char *b;
 auto signed r;
 auto signed char a[] = ("2K");
+b = (ARC_H_CONTROL_SEQUENCE_INTRODUCER);
+b = catna(a,cathy(b));
 r = (STDOUT_FILENO);
-b = catna(a,cathy(ARC_H_CONTROL_SEQUENCE_INTRODUCER));
 r = wrs(r,b,ct(b));
 rlse(b);
 b = (0x00);
