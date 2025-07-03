b5fad6e29aeae15e37fb1d8d9b92227ccae9c6e5
chris <chlds@yahoo.com>
2025-07-04 08:24:10 +0900

    Change

--- lib/cat/obj/src/cal_md.c
+++ lib/cat/obj/src/cal_md.c
@@ -1,25 +1,11 @@
-# define CAR_H
 # define CAT_H
-# define TIME_H
 # include "./../../../incl/config.h"
 
-signed char *(__cdecl cal_md(signed(args),void(*argp))) {
-auto signed char **w;
-auto signed char *b;
+signed(__cdecl cal_md(signed(args),void(*argp))) {
 auto signed r;
 auto signed m = (12);
-auto signed char a[] = ("AM");
-auto signed char p[] = ("PM");
-auto signed char *(e[]) = {
-a,
-p,
-0x00,
-};
-b = (0x00);
 r = cal_hr(args,argp);
-if(!(0x01+(r))) return(b);
-w = (e);
-if(!(r<(m))) w++;
-b = (*w);
-return(cathy(b));
+if(!(0x01+(r))) return(r);
+r = (r/(m));
+return(r);
 }
---
+++ lib/cat/obj/src/cv_md.c
@@ -0,0 +1,23 @@
+# define CAR_H
+# define CAT_H
+# include "./../../../incl/config.h"
+
+signed char *(__cdecl cv_md(signed(args),void(*argp))) {
+auto signed char **w;
+auto signed char *b;
+auto signed r;
+auto signed char a[] = ("AM");
+auto signed char p[] = ("PM");
+auto signed char *(e[]) = {
+a,
+p,
+0x00,
+};
+b = (0x00);
+r = cal_md(args,argp);
+if(!(0x01+(r))) return(b);
+w = (e);
+if(r) w++;
+b = (*w);
+return(cathy(b));
+}
