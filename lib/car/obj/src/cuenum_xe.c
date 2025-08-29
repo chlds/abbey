3cf247c36758a425c1c54715d22f770cfcf797ed
chris <chlds@yahoo.com>
2025-08-29 19:50:05 +0900

    Indexing

---
+++ lib/car/obj/src/cuenum_xe.c
@@ -0,0 +1,8 @@
+# define CAR_H
+# include "./../../../incl/config.h"
+
+signed(__cdecl cuenum_xe(signed char(*argp))) {
+if(!(lt(argp))) return(~0x00);
+if(cuenum(argp)) return(0x00);
+return(alte(0x01,cuenum_xe(argp+(lt(argp)))));
+}
