ecd18285c063845c8fd26c07a3c8f53f34ba9a46
chris <chlds@yahoo.com>
2025-08-31 10:02:35 +0900

    Fix

---
+++ lib/car/obj/src/anynonnum.c
@@ -0,0 +1,6 @@
+# define CAR_H
+# include "./../../../incl/config.h"
+
+signed(__cdecl anynonnum(signed char(*argp))) {
+return(cuenum_xxe(argp));
+}
---
+++ lib/car/obj/src/anynonsym.c
@@ -0,0 +1,6 @@
+# define CAR_H
+# include "./../../../incl/config.h"
+
+signed(__cdecl anynonsym(signed char(*argp))) {
+return(cuesym_xxe(argp));
+}
