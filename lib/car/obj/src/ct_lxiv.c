9fdf3d3a652b1ef7bda19c4b506dceb0b314b28e
chris <chlds@yahoo.com>
2025-09-04 15:03:50 +0900

    Fix

--- lib/car/obj/src/ct_lxiv.c
+++ lib/car/obj/src/ct_lxiv.c
@@ -3,10 +3,11 @@
 
 signed(__cdecl ct_lxiv(signed char(*argp))) {
 auto signed r;
+auto signed q = (CAR_H_EQ);
+auto signed char *e = (CAR_H_BASE_LXIV);
 r = cara(argp);
 if(!r) return(r);
-if(EQ(CAR_H_EQ,r)) return(0x01+(ct_lxiv(++argp)));
-r = ord(r,CAR_H_BASE_LXIV);
-if(r<(ct(CAR_H_BASE_LXIV))) return(0x01+(ct_lxiv(++argp)));
+if(EQ(q,r)) return(0x01+(ct_lxiv(++argp)));
+if(!(0x00<(0x01+(~ordi(r,e))))) return(0x01+(ct_lxiv(++argp)));
 return(ct_lxiv(++argp));
 }
--- lib/car/obj/src/dcd_vi_lxiv.c
+++ lib/car/obj/src/dcd_vi_lxiv.c
@@ -2,13 +2,11 @@
 # include "./../../../incl/config.h"
 
 signed(__cdecl dcd_vi_lxiv(signed(arg))) {
-auto signed char *b;
 auto signed r;
-AND(r,0x00);
-OR(r,CAR_H_EQ);
-if(EQ(r,arg)) return(0x00);
-b = (CAR_H_BASE_LXIV);
-r = ord(arg,b);
-if(!(r<(ct(b)))) return(0x00);
+auto signed q = (CAR_H_EQ);
+auto signed char *e = (CAR_H_BASE_LXIV);
+if(EQ(q,arg)) return(0x00);
+r = ordi(arg,e);
+if(r<(0x00)) return(0x00);
 return(r);
 }
