273292bf80e1e9882b83284032175748bc858213
chris <chlds@yahoo.com>
2025-09-05 15:16:50 +0900

    Add public header files

---
+++ incl/abbey.h
@@ -0,0 +1,8 @@
+# ifndef READY_FOR_ABBEY_H
+# define READY_FOR_ABBEY_H
+# define __cdecl __attribute__((ms_abi))
+# define __sysv __attribute__((sysv_abi))
+# include "./abbey_e.h"
+# include "./abbey_d.h"
+# include "./abbey_f.h"
+# endif
