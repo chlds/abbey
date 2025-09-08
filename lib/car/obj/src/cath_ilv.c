92d708c5179dddc245416c3c301a744bd5d35ca8
chris <chlds@yahoo.com>
2025-09-08 15:03:35 +0900

    Fix

--- lib/car/obj/src/cath_ilv.c
+++ lib/car/obj/src/cath_ilv.c
@@ -9,5 +9,6 @@ Interleave
 
 signed char *(__cdecl cath_ilv(signed(args),signed char(*argp))) {
 auto signed char a[] = ("\r\n");
+if(!(0x00<(args))) return(0x00);
 return(catna(a,cath_ilv_r(args,argp)));
 }
