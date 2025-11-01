e4abcad086d771157eadf61056ac4dd2ec0a1890
chris <chlds@yahoo.com>
2025-11-01 15:30:10 +0900

    Fix

--- lib/cat/obj/src/cv_tz_utc.c
+++ lib/cat/obj/src/cv_tz_utc.c
@@ -3,11 +3,9 @@
 # include "./../../../incl/config.h"
 
 signed char *(__cdecl cv_tz_utc(signed char(*argp))) {
-auto signed char *b;
 auto signed char c[] = ("+");
 auto signed char a[] = ("UTC");
-b = (0x00);
 if(!argp) return(argp);
 if(!(cmp(c,argp))) return(cathy(a));
-return(cath(a,argp,b));
+return(catta(a,argp));
 }
