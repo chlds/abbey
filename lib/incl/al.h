4cf12d5458751118c2ecfe1f02056f5a1a84d98e
chris <chlds@yahoo.com>
2026-01-26 00:00:00 +0900

    Add

--- lib/incl/al.h
+++ lib/incl/al.h
@@ -20,6 +20,12 @@ Refer at ./config.h
 # define XOR(A,B) A = (B^(A))
 # define NOT(A) A = (~(A))
 // Logical op.
+
+# define CII(A) A = (0x01+(~(A)))
+// CII (NEG) computes the two's complement of the operand by bitwise inversion followed by increment.
+// If the operand equals the minimum representable value, the result remains unchanged.
+// In standard C, signed integer overflow is undefined behavior and no signal is raised.
+
 # define ADD(A,B) A = (B+(A))
 # define MUL(A,B) A = (B*(A))
 // Expression
