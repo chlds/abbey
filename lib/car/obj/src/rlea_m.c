6d3a2f22fd3eff60faafa66413b65070393039e0
chris <chlds@yahoo.com>
2026-02-18 15:24:05 +0900

    Add

---
+++ lib/car/obj/src/rlea_m.c
@@ -0,0 +1,9 @@
+# define CAR_H
+# include "./../../../incl/config.h"
+
+signed **(__cdecl rlea_m(signed(**argp))) {
+auto signed r;
+r = rlse_m(argp);
+argp = (0x00);
+return(argp);
+}
---
+++ lib/car/obj/src/rlea_u.c
@@ -0,0 +1,9 @@
+# define CAR_H
+# include "./../../../incl/config.h"
+
+signed *(__cdecl rlea_u(signed(*argp))) {
+auto signed r;
+r = rlse_u(argp);
+argp = (0x00);
+return(argp);
+}
