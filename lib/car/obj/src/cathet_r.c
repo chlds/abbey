980837ac13d2931c85520da4c79096ec70bd6d95
chris <chlds@yahoo.com>
2026-04-14 23:11:55 +0900

    Fix

--- lib/car/obj/src/cathet_r.c
+++ lib/car/obj/src/cathet_r.c
@@ -3,10 +3,11 @@
 
 signed char **(__cdecl cathet_r(signed char(*argp))) {
 auto signed char **w;
+auto signed char *b;
 auto signed r;
 w = (0x00);
 r = cuet_xe(argp);
 if(!r) return(w);
-w = cathal_v_xe(cyne(argp,r));
-return(casse_v(w,cathet_r(argp+(r))));
+b = cyne(argp,r);
+return(cathol_xe(b,cathet_r(argp+(r))));
 }
--- lib/car/obj/src/cathev_r.c
+++ lib/car/obj/src/cathev_r.c
@@ -3,10 +3,11 @@
 
 signed char **(__cdecl cathev_r(signed char(*argp))) {
 auto signed char **w;
+auto signed char *b;
 auto signed r;
 w = (0x00);
 r = cuev_xe(argp);
 if(!r) return(w);
-w = cathal_v_xe(cyne(argp,r));
-return(casse_v(w,cathev_r(argp+(r))));
+b = cyne(argp,r);
+return(cathol_xe(b,cathev_r(argp+(r))));
 }
